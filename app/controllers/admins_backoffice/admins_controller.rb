7c4b33a80e42bd0aef408b61f50de4cba541d352
Nicolas Magalhaes <nicolas.magalhaes.clear@gmail.com>
2023-10-19 09:24:17 -0300

    new routes added and private methods added new create update params_admin set_admin verify_password

--- app/controllers/admins_backoffice/admins_controller.rb
+++ app/controllers/admins_backoffice/admins_controller.rb
@@ -1,19 +1,50 @@
 class AdminsBackoffice::AdminsController < AdminsBackofficeController
+  before_action :set_admin, only: [:edit, :update]
+  before_action :set_admin, only: [:edit, :update, :destroy]
+
   def index
     @admins = Admin.all
   end
 
+  def new
+    @admin = Admin.new
+  end
+
+  def create
+    @admin = Admin.new(params_admin)
+    if @admin.save
+      redirect_to admins_backoffice_admins_path, notice: "Administrador cadastrado com sucesso!"
+    else
+      render :new
+    end
+  end
+
   def edit
-    @admin = Admin.find(params[:id])
   end
 
   def update
-    admin = Admin.find(params[:id])
-    params_admin = params.require(:admin).permit(:email, :password, :password_confirmation)
-    if admin.update(params_admin)
-      redirect_to admins_backoffice_admins_path, notice: "Administrador atualizado com sucesso!"
+    if @admin.update(params_admin)
+      redirect_to admins_backoffice_admins_path, notice: "Administrador 
+    atualizado com sucesso!"
     else
       render :edit
+    end
+  end
+
+  private
+
+  def params_admin
+    params.require(:admin).permit(:email, :password, :password_confirmation)
+  end
+
+  def set_admin
+    @admin = Admin.find(params[:id])
+  end
+
+  def verify_password
+    if params[:admin][:password].blank? &&
+       params[:admin][:password_confirmation].blank?
+      params[:admin].extract!(:password, :password_confirmation)
     end
   end
 end
