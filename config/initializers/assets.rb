28c9fa3863d56268d26a5eead7fd8fd17b13492f
Nicolas Magalhaes <nicolas.magalhaes.clear@gmail.com>
2023-10-20 12:14:18 -0300

    assets precompile list updated

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,5 +10,5 @@ Rails.application.config.assets.version = "1.0"
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 Rails.application.config.assets.precompile += %w( img.png )
-Rails.application.config.assets.precompile += %w( font-awesome.css sb-admin-2.css metisMenu.css.map gentelella-custom.css )
-Rails.application.config.assets.precompile += %w( sb-admin-2.js gentelella-custom.js )
+Rails.application.config.assets.precompile += %w( font-awesome.css sb-admin-2.css metisMenu.css.map gentelella-custom.css surface.css welcome.css)
+Rails.application.config.assets.precompile += %w( sb-admin-2.js gentelella-custom.js surface.js )
