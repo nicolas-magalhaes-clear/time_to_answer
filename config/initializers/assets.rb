73d13bcdf88599c48a32815b094c4081dba1ec3b
Nicolas Magalhaes <nicolas.magalhaes.clear@gmail.com>
2023-10-23 12:30:18 -0300

    view complete

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,4 +11,5 @@ Rails.application.config.assets.version = "1.0"
 # folder are already added.
 Rails.application.config.assets.precompile += %w( img.png )
 Rails.application.config.assets.precompile += %w( font-awesome.css sb-admin-2.css metisMenu.css.map gentelella-custom.css surface.css welcome.css)
+Rails.application.config.assets.precompile += %w( question.js.erb )
 Rails.application.config.assets.precompile += %w( sb-admin-2.js gentelella-custom.js surface.js )
