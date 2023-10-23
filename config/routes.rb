004730f3132e62f0988a25a30c48f6049eec702c
Nicolas Magalhaes <nicolas.magalhaes.clear@gmail.com>
2023-10-23 09:40:59 -0300

    added route get search#questions

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   devise_for :users
   namespace :site do
     get 'welcome/index'
+    get 'search', to: 'search#questions'
   end  
   namespace :admins_backoffice do
     get 'welcome/index'
