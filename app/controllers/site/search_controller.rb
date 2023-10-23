35b296b4834035a8561277a8d83a27f811f676cd
Nicolas Magalhaes <nicolas.magalhaes.clear@gmail.com>
2023-10-23 09:36:03 -0300

    created search controller

---
+++ app/controllers/site/search_controller.rb
@@ -0,0 +1,5 @@
+class Site::SearchController < SiteController
+  def questions
+    @questions = Question.includes(:answers).where("lower(description) LIKE ?","%#{params[:term].downcase}%").page(params[:page])
+  end
+end
