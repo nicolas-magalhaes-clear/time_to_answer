9a0e2fc6dcab827562147e14b860862427549d59
Nicolas Magalhaes <nicolas.magalhaes.clear@gmail.com>
2023-10-24 12:30:02 -0300

    user and admin question count implement

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,6 +2,7 @@ class Question < ApplicationRecord
   belongs_to :subject, counter_cache: true, inverse_of: :questions
   has_many :answers
   accepts_nested_attributes_for :answers, reject_if: :all_blank, allow_destroy: true
+  after_create :set_statistic
 
   paginates_per 5
 
@@ -20,4 +21,10 @@ class Question < ApplicationRecord
         .where(subject_id: subject_id)
         .page(page)
     }
+
+  private
+
+  def set_statistic
+    AdminStatistic.set_event(AdminStatistic::EVENTS[:total_users])
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_10_24_133233) do
+ActiveRecord::Schema[7.1].define(version: 2023_10_24_150623) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -37,6 +37,13 @@ ActiveRecord::Schema[7.1].define(version: 2023_10_24_133233) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "admin_statistics", force: :cascade do |t|
+    t.string "event"
+    t.integer "value", default: 0
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "admins", force: :cascade do |t|
@@ -85,6 +92,15 @@ ActiveRecord::Schema[7.1].define(version: 2023_10_24_133233) do
     t.index ["user_id"], name: "index_user_profiles_on_user_id"
   end
 
+  create_table "user_statistics", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "right_questions", default: 0
+    t.integer "wrong_questions", default: 0
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_user_statistics_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -104,4 +120,5 @@ ActiveRecord::Schema[7.1].define(version: 2023_10_24_133233) do
   add_foreign_key "answers", "questions"
   add_foreign_key "questions", "subjects"
   add_foreign_key "user_profiles", "users"
+  add_foreign_key "user_statistics", "users"
 end
