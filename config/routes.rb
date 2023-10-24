a593eace4e4229da947190b659f7f505fe4c2c2f
Nicolas Magalhaes <nicolas.magalhaes.clear@gmail.com>
2023-10-24 10:14:40 -0300

    datepicker implement user data form redirect /backoffice to admin backoffice

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,7 @@ Rails.application.routes.draw do
   end
   devise_for :admins, :skip => [:registration]
   get 'welcome/index'
+  get 'backoffice', to: 'admins_backoffice/welcome#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root to: 'site/welcome#index'
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
